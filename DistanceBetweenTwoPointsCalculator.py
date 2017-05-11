e09ce811ffed06ff9ac5e2dc6127ce8df2fc6aad
moe moe <moe moe>
2017-05-11 13:04:02 -0700

    adding successful code

--- DistanceBetweenTwoPointsCalculator.py
+++ DistanceBetweenTwoPointsCalculator.py
@@ -8,7 +8,18 @@
 # Define Function below
 # be sure to return an integer
 
+
+import math
+
+def calculateDistanceBetween(x1,y1,x2,y2):
+    Distance=(((x2-x1)**2)+((y2-y1)**2))**(1/2)
+    Distance=round(Distance,2)
+    return Distance
+
+
+
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
     pass # remove or comment out this line if you wish to test the function
+    
